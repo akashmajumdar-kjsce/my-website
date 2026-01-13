038583f2f91eb1b8722b2800f856a69d5fc0d3d4
akashmajumdar-kjsce <akash.majumdar@somaiya.edu>
2026-01-13 10:17:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const toggleBtn = document.getElementById("themeToggle");
 
+// Toggle theme
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("light-theme");
+
+    if (document.body.classList.contains("light-theme")) {
+        localStorage.setItem("theme", "light");
+    } else {
+        localStorage.setItem("theme", "dark");
+    }
 });
+
+// Load saved theme
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light-theme");
+}
